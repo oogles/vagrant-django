5592cb724ff4c4b4a4fe3a60640d3f653ec75e53
Alex Church <alex@church.id.au>
2018-03-15 21:45:01 +1100

    Bumped version to 0.6.0.

--- provision/scripts/bootstrap.sh
+++ provision/scripts/bootstrap.sh
@@ -3,7 +3,7 @@
 # vagrant-django
 # Vagrant provisioning for Django projects.
 # https://github.com/oogles/vagrant-django
-# v0.5.1
+# v0.6.0
 
 # Ensure all provisioning scripts are executable
 chmod 774 -R /opt/app/src/provision/scripts/
